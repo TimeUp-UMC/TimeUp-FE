02dc50430cc9c41e63efaff40636819eb620c4e0
anit7215 <tina112703@gmail.com>
2025-07-04 01:33:12 +0900

    feat: 공용 컴포넌트-모달창 구현

--- Time-Up/src/components/common/Modal.tsx
+++ Time-Up/src/components/common/Modal.tsx
@@ -1,1 +1,49 @@
-//모달창
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+interface ModalProps {
+  onClose: () => void;
+  onConfirm?: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ onClose, onConfirm, children }) => {
+  return (
+    <View className="absolute inset-0 bg-black/40 justify-center items-center px-2.5">
+      <View
+        className="w-full rounded-2xl bg-[#F7F7FE]"
+        style={{
+          shadowColor: '#65696D',
+          shadowOffset: { width: 0, height: -2 },
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+          elevation: 4,
+        }}
+      >
+        <View className="px-6 pt-11 pb-9">
+          <Text className="text-[20px] leading-[28px] font-medium text-center tracking-[-0.4px]">
+            {children}
+          </Text>
+        </View>
+
+        <View className="px-6 pb-4 flex-row justify-between gap-4">
+          <TouchableOpacity
+            onPress={onClose}
+            className="flex-1 h-[48px] rounded-xl bg-[#C9CDD1] justify-center items-center"
+          >
+            <Text className="text-center text-black font-semibold">취소</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={onConfirm ?? onClose}
+            className="flex-1 h-[48px] rounded-xl bg-[#4D4DFF] justify-center items-center"
+          >
+            <Text className="text-center text-white font-semibold">확인</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Modal;
--- Time-Up/src/pages/OnboardingPage.tsx
+++ Time-Up/src/pages/OnboardingPage.tsx
@@ -1,10 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
+import Modal from '../components/common/Modal';
 
 
 export default function OnboardingPage() {
   const navigation = useNavigation();
+  const [open, setOpen] = useState(false);
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl text-blue-600 font-bold">온보딩</Text>
@@ -12,6 +15,12 @@ export default function OnboardingPage() {
         onPress={() => navigation.navigate('LoginPage')}>
         <Text className="text-white">로그인</Text>
       </TouchableOpacity>
+      <Button title="모달 열기" onPress={() => setOpen(true)} />
+      {open && (
+        <Modal onClose={() => setOpen(false)}>
+            {"지금까지 입력하신 내용을 \n제출하시겠습니까?"}
+        </Modal>
+      )}
     </View>
   );
 }
